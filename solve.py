9d88f528791ce5765e0ab3422b3fffa283f8b00e
Artur <magisterbrownie@gmail.com>
2024-02-23 00:14:27 +0100

    branch prunning done

--- solve.py
+++ solve.py
@@ -1,28 +1,63 @@
 from games_base import DotsAndBoxes
 import random
 import time
+lookup = 0
+# MiniMax ----------------------------
+def minimax(state, depth: int, mini=float("-inf"), maxi=float("inf")): 
+    global lookup
+    lookup+=1
+    if state.terminal():
+        return state.reward() * float("inf"), None
+    if depth == 0:
+        return state.reward(), None
 
-game = DotsAndBoxes(5,5)
+    fact = None 
+    if state.player == 1:
+        best = float("-inf")
+        for act in state.actions():
+            nmin = best # mini
+            value, _ = minimax(state.transition(act), depth-1, nmin, maxi)
+            if value>best:
+                fact = act
+                best = value
+                if value>maxi:
+                    return best, act
+    else:
+        best = float("inf")
+        for act in state.actions():
+            nmax = best # maxi
+            value, _ = minimax(state.transition(act), depth-1, mini, nmax)
+            if value<best:
+                fact = act
+                best = value
+                if value<mini:
+                    return best, act
+
+    return best, act
+
+
+game = DotsAndBoxes(3,1)
 print(game)
 
 acts = game.actions()
-while not game.terminal():
+if True:
+    while not game.terminal():
+        
+        lookup = 0
+        #a = random.choice(acts)
+        val, a = minimax(game, 3)
+        print("--------------------------------")
+        print(f"Expectation: {val}")
+        print(game.player)
+        game = game.transition(a)
+        print(a)
+        print(game.points)
+        print(game)
+        print(f"Lookup: {lookup}")
+        #s = input("dd")
+        time.sleep(0.5)
+        acts = game.actions()
     
-    a = random.choice(acts)
-    print("--------------------------------")
-    print(game.player)
-    game = game.transition(a)
-    print(a)
-    print(game.points)
-    print(game)
-    #s = input("dd")
-    time.sleep(0.5)
-    acts = game.actions()
 
 
-
-# MiniMax ----------------------------
-def minimax(state, direct): 
-    return 0
-
-
+#vl = minimax(game, 4)
